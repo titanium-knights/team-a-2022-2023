6b08382bc6a027f7744d08fa5f3097ee515b5ebc
alisona <alile24@bergen.org>
2022-10-07 10:56:01 -0400

    lift start

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Lift.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.util;
+import com.qualcomm.robotcore.hardware.*;
+
+public class Lift {
+    DcMotor motor;
+
+    public static int HIGH_POSITION = 100;
+    public static int MID_POSITION = 50;
+}
