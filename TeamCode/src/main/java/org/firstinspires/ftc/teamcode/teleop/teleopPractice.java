216687a54736eb46b1907facdab6755c3ba320c0
riajk <riajkamat@gmail.com>
2022-10-07 10:37:08 -0400

    Created teleop file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleopPractice.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.teleop;
+//import com.acmerobotics.dashboard.FtcDashboard;
+//import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+//import com.acmerobotics.dashboard.config.Config;
+@TeleOp
+public class teleopPractice extends OpMode {
+    //MecanumDrive drive;
+}
