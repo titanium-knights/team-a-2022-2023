c13e50f52f2134fd5a15c4472aea0923cf218b81
alisona <alile24@bergen.org>
2022-10-07 11:09:14 -0400

    added comments to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopPractice.java
@@ -8,11 +8,25 @@ import org.firstinspires.ftc.teamcode.util.*;
 //import com.acmerobotics.dashboard.config.Config;
 @TeleOp
 public class TeleopPractice extends OpMode {
-    MecanumDrive drive;
-    public void init() {
+    MecanumDrive drive; // aidawkfafhiuawf
+    Claw claw;
+    //woot
+    public static double DRIVE_SPEED = 1; //idk we can play around w this
 
+    public void init() { //everything when you press the play button before u start goes here (INITialize, get it?)
+        drive = new MecanumDrive(hardwareMap);
+        claw = new Claw(hardwareMap);
     }
-    public void loop() {
+    public void loop() { //gamepad buttons that call util methods go here
+        //there are 2 gamepads (gamepad1 & gamepad2, start typing gamepad1 to see its buttons (it's the same as gamepad2)
 
+        //driving
+        drive.teleOpRobotCentric(gamepad1, DRIVE_SPEED); //go drive vroom
+
+        //claw
+        //[add claw stuff here!]
+
+        //lift
+        //[add lift stuff here!]
     }
 }
