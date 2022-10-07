f3e87e44bfba5847e142cd1437ebd54a2c714320
Ari Kamat <arijkamat@gmail.com>
2022-10-07 12:22:04 -0400

    Properly nested roadrunner files, inserted documented mecdrive code, and fixed issues with config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopPractice.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.teleop;
 //import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.util2.*;
+import org.firstinspires.ftc.teamcode.util.*;
 
 //import com.acmerobotics.dashboard.config.Config;
 @TeleOp
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/CONFIG.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.util;
+
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class CONFIG {
+    //drivetrain wheels :OOO
+    public static String FRONT_LEFT = "fl";
+    public static String FRONT_RIGHT = "fr";
+    public static String BACK_LEFT = "bl";
+    public static String BACK_RIGHT = "br";
+
+    //subsystems
+    public static String clawServo = "claw";
+
+    public static String liftMotor = "lift";
+
+}
