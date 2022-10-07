19bd8bae86f218964aeee66fa46572740601abee
alisona <alile24@bergen.org>
2022-10-07 11:19:26 -0400

    config class :OO (to get the names better)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Claw.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.util;
 import com.qualcomm.robotcore.hardware.*;
 
-
 public class Claw {
     public Servo claw; //servo or motor???
     public static double openPos = 1; //open and ready to take a cone or TSE
@@ -9,8 +8,8 @@ public class Claw {
     public static double closedConePos = .5; //closed for cone
     public static double closedTSEPos = .4; //closed for TSE
 
-    public Claw(HardwareMap hardwareMap) {
-        this.claw = hardwareMap.servo.get("claw");
+    public Claw(HardwareMap hmap) {
+        this.claw = hmap.servo.get(Config.clawServo);
     }
 
     public boolean isClosedCone() { //check if closed
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Config.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class Config {
+    public static String clawServo = "claw";
+
+
+}
