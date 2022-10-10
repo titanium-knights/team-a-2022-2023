d815008dfb5969ef647fbe278fa2496d7683db0a
riajk <riajkamat@gmail.com>
2022-10-10 17:06:50 -0400

    adding values to gamepad1. add values to right bumper, left bumper, x and a.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopPractice.java
@@ -14,12 +14,22 @@ public class TeleopPractice extends OpMode {
     Lift lift;
     //woot
     public static double DRIVE_SPEED = 1; //idk we can play around w this
+    public static double liftSetPower = 1;
 
     public void init() { //everything when you press the play button before u start goes here (INITialize, get it?)
         drive = new MecanumDrive(hardwareMap);
         claw = new Claw(hardwareMap);
     }
     public void loop() { //gamepad buttons that call util methods go here
+        //driving
+        drive.teleOpRobotCentric(gamepad1, DRIVE_SPEED); //go drive vroom
+
+        //claw
+        //[add claw stuff here!]
+
+        //lift
+        //[add lift stuff here!]
+
         //there are 2 gamepads (gamepad1 & gamepad2, start typing gamepad1 to see its buttons (it's the same as gamepad2)
         if (gamepad1.right_bumper){ //arm up
             lift.setPower(1);
@@ -33,19 +43,10 @@ public class TeleopPractice extends OpMode {
         if (gamepad1.a) { //lift arm down preset
             lift.setPower(-0.5); //placeholder for preset here. Add ACTUAL preset value!!
         }
-        if (gamepad1.y) { //open claw preset
-            lift.setPower(0.5); //placeholder for preset here. Add ACTUAL preset value!!
+        //if (gamepad1.y) { //open claw preset
+            //clawLift.setPower(0.5); //placeholder for preset here. Add ACTUAL preset value!!
         }
-        if (gamepad1.b) {//close claw preset
-            lift.setPower(-0.5); //placeholder for preset here. Add ACTUAL preset value!!
+        //if (gamepad1.b) {//close claw preset
+            //clawLift.setPower(-0.5); //placeholder for preset here. Add ACTUAL preset value!!
         }
-        //driving
-\       drive.teleOpRobotCentric(gamepad1, DRIVE_SPEED); //go drive vroom
 
-        //claw
-        //[add claw stuff here!]
-
-        //lift
-        //[add lift stuff here!]
-    }
-}
