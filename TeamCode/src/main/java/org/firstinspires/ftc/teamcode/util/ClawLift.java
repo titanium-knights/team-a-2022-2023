c98d4fd137407ac51a993b58c0d81d41460e6673
alisona <alile24@bergen.org>
2022-10-11 18:01:30 -0400

    fixed claw lift to be a servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ClawLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ClawLift.java
@@ -3,13 +3,13 @@ package org.firstinspires.ftc.teamcode.util;
 import com.qualcomm.robotcore.hardware.*;
 
 public class ClawLift {
-    public DcMotor liftMotor;
+    public Servo liftMotor;
 
     public int downPos = 100;
     public int upPos = 200;
 
     public ClawLift(HardwareMap hmap) {
-        this.liftMotor = hmap.dcMotor.get(CONFIG.clawLift);
+        this.liftMotor = hmap.servo.get(CONFIG.clawLift);
     }
 
 
