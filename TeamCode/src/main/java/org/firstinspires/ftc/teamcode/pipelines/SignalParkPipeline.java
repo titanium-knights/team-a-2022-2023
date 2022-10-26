c738d9cd88ab5bbb77cf4eec55495527b2695103
Alice Zhang <anz0063@gmail.com>
2022-10-25 23:28:34 -0400

    i think the pipeline simulator works now??

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/SignalParkPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/SignalParkPipeline.java
@@ -1,277 +1,222 @@
-//some code taken from EasyOpenCV SkystoneDeterminationExample
+
 package org.firstinspires.ftc.teamcode.pipelines;
 
-        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-        import org.opencv.core.Core;
-        import org.opencv.core.Mat;
-        import org.opencv.core.Point;
-        import org.opencv.core.Rect;
-        import org.opencv.core.Scalar;
-        import org.opencv.imgproc.Imgproc;
-        import org.openftc.easyopencv.OpenCvCamera;
-        import org.openftc.easyopencv.OpenCvCameraFactory;
-        import org.openftc.easyopencv.OpenCvCameraRotation;
-        import org.openftc.easyopencv.OpenCvInternalCamera;
-        import org.openftc.easyopencv.OpenCvPipeline;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.Arrays;
+
+/*
+ * This sample demonstrates a basic (but battle-tested and essentially
+ * 100% accurate) method of detecting the skystone when lined up with
+ * the sample regions over the first 3 stones.
+ */
+
+public class SignalParkPipeline extends OpenCvPipeline {
+    public SignalParkPipeline(Telemetry telemetry) {
+        this.telemetry = telemetry;
+    }
+
+    private volatile SignalZone currentZone = SignalZone.ZONE_ONE;
+
+    public enum SignalZone {
+        ZONE_ONE,
+        ZONE_TWO,
+        ZONE_THREE
+    }
 
     /*
-     * This sample demonstrates a basic (but battle-tested and essentially
-     * 100% accurate) method of detecting the skystone when lined up with
-     * the sample regions over the first 3 stones.
+     * Some color constants
+     */
+    static final Scalar PURPLE = new Scalar(168, 25, 250);
+    static final Scalar GREEN = new Scalar(0, 255, 0);
+    static final Scalar ORANGE = new Scalar(250, 136, 25);
+
+    /*
+     * The core values which define the location and size of the sample regions
      */
 
+    //PLACE ALL REGIONS IN THE SAME PLACE (?)
+    static final Point REGION_TOPLEFT_ANCHOR_POINT = new Point(125, 98);
 
-public class SignalParkPipeline extends OpenCvPipeline{
-    OpenCvInternalCamera phoneCam;
+    static final int REGION_WIDTH = 50;
+    static final int REGION_HEIGHT = 50;
+    //take the average of pixels and match it upn
 
-    public void runOpMode() {
-        /**
-         * NOTE: Many comments have been omitted from this sample for the
-         * sake of conciseness. If you're just starting out with EasyOpenCv,
-         * you should take a look at {@link InternalCamera1Example} or its
-         * webcam counterpart, {@link WebcamExample} first.
+    /*
+     * Points which actually define the sample region rectangles, derived from above values
+     *
+     * Example of how points A and B work to define a rectangle
+     *
+     *   ------------------------------------
+     *   | (0,0) Point A                    |
+     *   |                                  |
+     *   |                                  |
+     *   |                                  |
+     *   |                                  |
+     *   |                                  |
+     *   |                                  |
+     *   |                  Point B (70,50) |
+     *   ------------------------------------
+     *
+     */
+    Point region1_pointA = new Point(
+            REGION_TOPLEFT_ANCHOR_POINT.x,
+            REGION_TOPLEFT_ANCHOR_POINT.y);
+    Point region1_pointB = new Point(
+            REGION_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+            REGION_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+
+
+    /*
+     * Working variables
+     */
+    Mat region1_Hue;
+    Mat region1_Sat;
+    Mat HSV = new Mat();
+    Mat Hue = new Mat();
+    Mat Sat = new Mat();
+    double avg1;
+    Telemetry telemetry;
+
+    /*
+     * This function takes the RGB frame, converts to HSV,
+     * and extracts the Hue and Sat channels to the 'Hue' and 'Sat' variables
+     */
+    void inputToHSV(Mat input) {
+        Imgproc.cvtColor(input, HSV, Imgproc.COLOR_RGB2HSV);
+        Core.extractChannel(HSV, Hue, 0);
+        Core.extractChannel(HSV, Sat, 1);
+        /*
+         * Submats are a persistent reference to a region of the parent
+         * buffer. Any changes to the child affect the parent, and the
+         * reverse also holds true.
          */
+        region1_Hue = Hue.submat(new Rect(region1_pointA, region1_pointB));
+        region1_Sat = Sat.submat(new Rect(region1_pointA, region1_pointB));
+    }
 
-        phoneCam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
-
-        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                phoneCam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_LEFT);
-            }
-
-            @Override
-            public void onError(int errorCode) {
-                /*
-                 * This will be called if the camera could not be opened
-                 */
-            }
-        });
+    @Override
+    public void init(Mat firstFrame) {
+        /*
+         * We need to call this in order to make sure the 'Cb'
+         * object is initialized, so that the submats we make
+         * will still be linked to it on subsequent frames. (If
+         * the object were to only be initialized in processFrame,
+         * then the submats would become delinked because the backing
+         * buffer would be re-allocated the first time a real frame
+         * was crunched)
+         */
+        inputToHSV(firstFrame);
 
     }
 
-     public enum SignalZone {
-            ZONE_ONE,
-            ZONE_TWO,
-            ZONE_THREE
+    @Override
+    public Mat processFrame(Mat input) {
+
+        inputToHSV(input);
+
+        /*
+         * Compute the average pixel value of each submat region. We're
+         * taking the average of a single channel buffer, so the value
+         * we need is at index 0. We could have also taken the average
+         * pixel value of the 3-channel image, and referenced the value
+         * at index 2 here.
+         */
+
+        double sum = 0;
+        int count = 0;
+
+        for (int hueH = 0; hueH < region1_Hue.height(); hueH++) {
+            for (int hueW = 0; hueW < region1_Hue.width(); hueW++) {
+                if (region1_Sat.get(hueH, hueW)[0] > 88) {
+                    sum += region1_Hue.get(hueH, hueW)[0];
+                    count++;
+                }
+            }
         }
 
-        //colors of sleeve
-        //green already created
-        //static final Scalar MAGENTA = new Scalar();
-
-
-        /*
-         * Some color constants
-         */
-        static final Scalar BLUE = new Scalar(0, 0, 255);
-        static final Scalar GREEN = new Scalar(0, 255, 0);
-
-        /*
-         * The core values which define the location and size of the sample regions
-         */
-
-        //PLACE ALL REGIONS IN THE SAME PLACE (?)
-        static final Point REGION_TOPLEFT_ANCHOR_POINT = new Point(109, 98);
-
-        static final int REGION_WIDTH = 20;
-        static final int REGION_HEIGHT = 50;
-        //take the average of pixels and match it upn
-
-        /*
-         * Points which actually define the sample region rectangles, derived from above values
-         *
-         * Example of how points A and B work to define a rectangle
-         *
-         *   ------------------------------------
-         *   | (0,0) Point A                    |
-         *   |                                  |
-         *   |                                  |
-         *   |                                  |
-         *   |                                  |
-         *   |                                  |
-         *   |                                  |
-         *   |                  Point B (70,50) |
-         *   ------------------------------------
-         *
-         */
-        Point region1_pointA = new Point(
-                REGION_TOPLEFT_ANCHOR_POINT.x,
-                REGION_TOPLEFT_ANCHOR_POINT.y);
-        Point region1_pointB = new Point(
-                REGION_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
-
-        /*
-         * Working variables
-         */
-        Mat region1_Cb;
-        Mat YCrCb = new Mat();
-        Mat Cb = new Mat();
-        int avg1;
-
-        // Volatile since accessed by OpMode thread w/o synchronization
-        private volatile SignalZone position = SignalZone.ZONE_ONE;
-
-        /*
-         * This function takes the RGB frame, converts to YCrCb,
-         * and extracts the Cb channel to the 'Cb' variable
-         */
-        void inputToCb(Mat input) {
-            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-            Core.extractChannel(YCrCb, Cb, 2);
+//        find average hue of the region (neglecting the pixels that don't
+//        surpass a certain threshold in saturation)
+        if (count == 0) {
+            avg1 = 0;
+        } else {
+            avg1 = sum / count;
+        }
+        if (telemetry != null) {
+            telemetry.addData("AVG: ", avg1);
+            telemetry.addData("Count: ", count);
+            telemetry.update();
         }
 
-        @Override
-        public void init(Mat firstFrame) {
-            /*
-             * We need to call this in order to make sure the 'Cb'
-             * object is initialized, so that the submats we make
-             * will still be linked to it on subsequent frames. (If
-             * the object were to only be initialized in processFrame,
-             * then the submats would become delinked because the backing
-             * buffer would be re-allocated the first time a real frame
-             * was crunched)
-             */
-            inputToCb(firstFrame);
+        /*
+         * Find which hue each region is closest to
+         * Draw a rectangle in that color
+         */
+        double[] hues = {138, 60, 15};
 
-            /*
-             * Submats are a persistent reference to a region of the parent
-             * buffer. Any changes to the child affect the parent, and the
-             * reverse also holds true.
-             */
-            region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
+//            double closestHue = Arrays.stream(hues).map(hue -> {
+//                return Math.min(Math.abs(avg1-hue), 180-Math.abs(avg1-hue));
+//            }).min().getAsDouble();
+        double minDistance = Double.POSITIVE_INFINITY;
+        int minIndex = 0;
+        for (int i = 0; i < 3; i++) {
+            double dis = Math.min(Math.abs(avg1 - hues[i]), 180 - Math.abs(avg1 - hues[i]));
+            if (dis < minDistance) {
+                minDistance = dis;
+                minIndex = i;
+            }
         }
 
-        @Override
-        public Mat processFrame(Mat input) {
-            /*
-             * Overview of what we're doing:
-             *
-             * We first convert to YCrCb color space, from RGB color space.
-             * Why do we do this? Well, in the RGB color space, chroma and
-             * luma are intertwined. In YCrCb, chroma and luma are separated.
-             * YCrCb is a 3-channel color space, just like RGB. YCrCb's 3 channels
-             * are Y, the luma channel (which essentially just a B&W image), the
-             * Cr channel, which records the difference from red, and the Cb channel,
-             * which records the difference from blue. Because chroma and luma are
-             * not related in YCrCb, vision code written to look for certain values
-             * in the Cr/Cb channels will not be severely affected by differing
-             * light intensity, since that difference would most likely just be
-             * reflected in the Y channel.
-             *
-             * After we've converted to YCrCb, we extract just the 2nd channel, the
-             * Cb channel. We do this because stones are bright yellow and contrast
-             * STRONGLY on the Cb channel against everything else, including SkyStones
-             *
-             * We then take the average pixel value of 3 different regions on that Cb
-             * channel, one positioned over each stone. The brightest of the 3 regions
-             * is where we assume the SkyStone to be, since the normal stones show up
-             * extremely darkly.
-             *
-             * We also draw rectangles on the screen showing where the sample regions
-             * are, as well as drawing a solid rectangle over top the sample region
-             * we believe is on top of the SkyStone.
-             *
-             * In order for this whole process to work correctly, each sample region
-             * should be positioned in the center of each of the first 3 stones, and
-             * be small enough such that only the stone is sampled, and not any of the
-             * surroundings.
-             */
-
-            /*
-             * Get the Cb channel of the input frame after conversion to YCrCb
-             */
-            inputToCb(input);
-
-            /*
-             * Compute the average pixel value of each submat region. We're
-             * taking the average of a single channel buffer, so the value
-             * we need is at index 0. We could have also taken the average
-             * pixel value of the 3-channel image, and referenced the value
-             * at index 2 here.
-             */
-
-            //average color of
-            avg1 = (int) Core.mean(region1_Cb).val[0];
-
-            //matchn colors
-
-            /*
-             * Draw a rectangle showing sample region on the screen.
-             * Simply a visual aid. Serves no functional purpose.
-             */
+        if (minIndex == 0) { //closest to purple
             Imgproc.rectangle(
                     input, // Buffer to draw on
                     region1_pointA, // First point which defines the rectangle
                     region1_pointB, // Second point which defines the rectangle
-                    BLUE, // The color the rectangle is drawn in
+                    PURPLE, // The color the rectangle is drawn in
+                    2);
+            currentZone = SignalZone.ZONE_ONE;
+        } else if (minIndex == 1) { //closest to green
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    region1_pointA, // First point which defines the rectangle
+                    region1_pointB, // Second point which defines the rectangle
+                    GREEN, // The color the rectangle is drawn in
+                    2);
+            currentZone = SignalZone.ZONE_TWO;
+
+        } else {  //closest to orange
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    region1_pointA, // First point which defines the rectangle
+                    region1_pointB, // Second point which defines the rectangle
+                    ORANGE, // The color the rectangle is drawn in
                     2); // Thickness of the rectangle lines
+            currentZone = SignalZone.ZONE_THREE;
 
 
-            /*
-             * Now that we found the max, we actually need to go and
-             * figure out which sample region that value was from
-             */
-//            if (avg1 == avg1) // Was it from region 1?
-//            {
-//                position = SignalZone.ZONE_ONE; // Record our analysis
-//
-//                /*
-//                 * Draw a solid rectangle on top of the chosen region.
-//                 * Simply a visual aid. Serves no functional purpose.
-//                 */
-//                Imgproc.rectangle(
-//                        input, // Buffer to draw on
-//                        region1_pointA, // First point which defines the rectangle
-//                        region1_pointB, // Second point which defines the rectangle
-//                        GREEN, // The color the rectangle is drawn in
-//                        -1); // Negative thickness means solid fill
-//            } else if (max == avg2) // Was it from region 2?
-//            {
-//                position = SignalZone.ZONE_TWO; // Record our analysis
-//
-//                /*
-//                 * Draw a solid rectangle on top of the chosen region.
-//                 * Simply a visual aid. Serves no functional purpose.
-//                 */
-//                Imgproc.rectangle(
-//                        input, // Buffer to draw on
-//                        region2_pointA, // First point which defines the rectangle
-//                        region2_pointB, // Second point which defines the rectangle
-//                        GREEN, // The color the rectangle is drawn in
-//                        -1); // Negative thickness means solid fill
-//            } else if (max == avg3) // Was it from region 3?
-//            {
-//                position = SignalZone.ZONE_THREE; // Record our analysis
-//
-//                /*
-//                 * Draw a solid rectangle on top of the chosen region.
-//                 * Simply a visual aid. Serves no functional purpose.
-//                 */
-//                Imgproc.rectangle(
-//                        input, // Buffer to draw on
-//                        region3_pointA, // First point which defines the rectangle
-//                        region3_pointB, // Second point which defines the rectangle
-//                        GREEN, // The color the rectangle is drawn in
-//                        -1); // Negative thickness means solid fill
-//            }
-
-            /*
-             * Render the 'input' buffer to the viewport. But note this is not
-             * simply rendering the raw camera feed, because we called functions
-             * to add some annotations to this buffer earlier up.
-             */
-            return input;
         }
 
         /*
-         * Call this from the OpMode thread to obtain the latest analysis
+         * Render the 'input' buffer to the viewport. But note this is not
+         * simply rendering the raw camera feed, because we called functions
+         * to add some annotations to this buffer earlier up.
          */
-        public SignalZone getAnalysis() {
-            return position;
-        }
+        return input;
     }
+
+
+    /*
+     * Call this from the OpMode thread to obtain the latest analysis
+     */
+    public SignalZone getAnalysis() {
+        return currentZone;
+    }
+}
