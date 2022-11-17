6316aa12bf945dd2fe0be7eb600376733e7251cc
alisona <alile24@bergen.org>
2022-11-17 17:27:07 -0500

    configed dt and claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/CONFIG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/CONFIG.java
@@ -6,19 +6,22 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class CONFIG {
     //drivetrain wheels :OOO
-    public static String FRONT_LEFT = "fl"; //ch 0
-    public static String FRONT_RIGHT = "fr"; //eh 0
-    public static String BACK_LEFT = "bl"; //ch 1
-    public static String BACK_RIGHT = "br"; //eh 1
+    public static String FRONT_LEFT = "fl"; //ch 3
+    public static String FRONT_RIGHT = "fr"; //ch 2
+    public static String BACK_LEFT = "bl"; //ch 0
+    public static String BACK_RIGHT = "br"; //ch 1
 
     //webcam
     public static String webcam = "Webcam 1";
 
     //subsystems
-    public static String clawServo = "claw"; //ch 5
+    public static String clawServo = "claw"; //ch 0
 
-    public static String liftMotor = "lift"; //ch 2, encoder also connected to this port
+    public static String liftMotor = "lift"; //
 
-    public static String clawLift = "clawLift"; //eh5
+    public static String clawLift = "clawLift"; //
+
+    public static String liftMotorRight = "lmr"; //exp
+    public static String liftMotorLeft = "lml";  //exp
 
 }
