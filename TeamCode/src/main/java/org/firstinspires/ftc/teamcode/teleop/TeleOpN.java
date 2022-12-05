7dd74671ba1fa63912ff9be378ed104a6343f587
riajk <riajkamat@gmail.com>
2022-12-05 17:25:56 -0500

    rr attempt :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpN.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpN.java
@@ -41,32 +41,17 @@ public class TeleOpN extends OpMode {
         if(Math.abs(gamepad2.left_stick_y)>0.1){
              lift.setPower(gamepad1.left_stick_y);
         }
+        //claw
+        if (gamepad1.a) {
+            claw.open();
+        }
+        if (gamepad1.y){
+            claw.closeCone();
+        }
 
 
     }
     //we decided that right trigger would be lift up, left trigger lift down, a claw open, b claw close
 
-    public void loop() { //gamepad buttons that call util methods go here
-       if (gamepad1.a) {
-            isSlowmode = !isSlowmode;
-        }
-
-        if (!isSlowmode) {
-            drive.teleOpRobotCentric(gamepad1, DRIVE_SPEED); //go drive vroom
-        } else {
-            drive.teleOpRobotCentric(gamepad1, DRIVE_SPEED_SLOWMODE);
-        }
-
-        telemetry.addData("Slow mode on?:", isSlowmode);
-
-    //claw
-        if (gamepad1.a) {
-        claw.open();
-    }
-        if (gamepad1.y){
-            claw.closeCone();
-        }
-        // lift stuff
-        }
 }
 
