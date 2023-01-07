2765f2802ce23733f5b571c493674c726e5e4bd6
alisona <alile24@bergen.org>
2023-01-06 22:48:06 -0500

    fix ports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/CONFIG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/CONFIG.java
@@ -6,10 +6,10 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class CONFIG {
     //drivetrain wheels :OOO
-    public static String FRONT_LEFT = "fl"; //ch 0
-    public static String FRONT_RIGHT = "fr"; //ch 1
-    public static String BACK_LEFT = "bl"; //ch 3
-    public static String BACK_RIGHT = "br"; //ch 2
+    public static String FRONT_LEFT = "fl"; //ch 2
+    public static String FRONT_RIGHT = "fr"; //ch 3
+    public static String BACK_LEFT = "bl"; //ch 1
+    public static String BACK_RIGHT = "br"; //ch 0
 
     //webcam
     public static String webcam = "Webcam 1";
